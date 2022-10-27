474745c75e04fc4391112ccd1e3f27fc44378a87
Ilyushchenya <dimailyushen@gmail.com>
2022-10-27 22:59:44 +0300

    third day ending

--- testproj/news/models.py
+++ testproj/news/models.py
@@ -1,11 +1,34 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%\\d')
-    is_published = models.BooleanField(default=True)
-    we_are_fucked = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='наименование')
+    content = models.TextField(blank=True, verbose_name='контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Статус публикации')
+    we_are_fucked = models.BooleanField(default=True, verbose_name='Мы в дерьме ?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+    
+        
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Наименование категории")
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+    
+        
+        
--- testproj/news/views.py
+++ testproj/news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.order_by("-id")
+    # res = "<h1>Список новостей:</h1>"
+    # for item in news:
+    #     res += f"<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n"
+        
+    return render(request, 'news/VUZ_page.html', {'news': news, 'title': 'Список новостой'})
 
 def test(request):
     return HttpResponse('<h1>Тестовая страница</h1>')

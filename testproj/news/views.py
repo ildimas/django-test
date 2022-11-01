64e2c2f021518ef88db968a47ab2b58284570053
Ilyushchenya <dimailyushen@gmail.com>
2022-11-01 22:35:53 +0300

    latrst commit

--- testproj/news/views.py
+++ testproj/news/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News, Category
+import folium
+from dadata import Dadata
+
+token = 'C2313134123'
+secret = 'QWERTFDBDF2134'
 
 def index(request):
     news = News.objects.order_by("-id")
@@ -18,4 +23,11 @@ def view_news(request, news_id):
     news_item = News.objects.get(pk=news_id)
     return render(request, 'news\\view_news.html', {'news_item': news_item})
 def misha(request):
-    return render(request, 'news\index.html')
+    #Делаем карту
+    m = folium.Map(location=[55.9, 37.7])
+    
+    folium.Marker([5.594, -0.219]).add_to(m)
+    
+    m = m._repr_html_()
+    context = {'m': m,}
+    return render(request, 'news\index.html', context=context)
